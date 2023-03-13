a4c65d6376973ebe6ce27112b54058ab3137de8a
Jieun Park <121587779+jieun000@users.noreply.github.com>
2023-03-13 23:46:27 +0900

    Add files via upload

---
+++ Start_Java/A_basic_variable_dataType_controlStatements_arrangement/src/a/A_basic.java
@@ -0,0 +1,19 @@
+package a;
+
+public class A_basic {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java World");
+		
+		System.out.print("소환사 ");
+		System.out.println("협곡에");
+		System.out.println("오신 것을");
+		System.out.println("환영합니다.");
+		
+		System.out.printf("Jieun님 ");
+		System.out.printf("Excellent");
+
+	}
+
+}
