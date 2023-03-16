bbb939dbf773b930564a7a1b36c9dc4230277c36
jieun <mondieu0@naver.com>
2023-03-16 23:06:59 +0900

    basic2

---
+++ A_basic_variable_dataType_controlStatements_arrangement/src/a_basic_variable/B_variable.java
@@ -0,0 +1,18 @@
+package a_basic_variable;
+
+public class B_variable {
+
+	public static void main(String[] args) {
+
+		int num;
+		int num2 = 20, num3 = 30;
+		// System.out.println(num); 변수 num의 내용이 없어 오류
+		
+		num = 10; // 공간 num에 10을 넣어줌
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+		
+	}
+
+}
