024c5c9fb2986838e0d572489889d0a8264dec14
jieun <mondieu0@naver.com>
2023-03-29 20:41:54 +0900

    for

---
+++ A_basic_variable_dataType_controlStatements_arrangement/src/b_controlStatement/B_for_0.java
@@ -0,0 +1,16 @@
+package b_controlStatement;
+
+public class B_for_0 {
+
+	public static void main(String[] args) {
+
+		int i; // 반복용 변수 i, j
+		
+		// for(int i=1; i<=10; i++)
+		for(i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
