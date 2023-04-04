e5a4f5f92b306dec2de14c4bec5b20e0b95e0b4a
jieun <mondieu0@naver.com>
2023-04-04 23:16:23 +0900

    for_ifPractice2

---
+++ A_basic_variable_dataType_controlStatements_arrangement/src/b_controlStatement/B_if_Practice2_average.java
@@ -0,0 +1,38 @@
+package b_controlStatement;
+
+import java.util.Scanner;
+
+public class B_if_Practice2_average {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A; // 국어
+		int B; // 영어
+		int C; // 수학
+		double avg; // 평균
+
+		System.out.print("국어 점수를 입력하시오: ");
+			A = sc.nextInt();
+		System.out.print("영어 점수를 입력하시오: ");
+			B = sc.nextInt();
+		System.out.print("수학 점수를 입력하시오: ");
+			C = sc.nextInt();		
+			
+		avg = (A + B + C) / 3.0;	
+		// 형 변환: avg = (double)(A + B + C) / 3.0;
+		
+		if(avg >= 70) {
+			if(A<60 || B<60 || C<60) {
+				System.out.println("과락");
+			} else {
+				System.out.println("합격");
+			}
+		} else {
+			System.out.println("불합격");
+		}
+		
+	}
+	
+}
